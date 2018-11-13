0f9848ebb1172790505f129086f4975667934a12
MetroWind <chris.corsair@gmail.com>
2018-11-13 15:37:58 -0800

    Add context menu support.

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-function restoreMostRecent(sessions)
+function restoreMostRecentFrom(sessions)
 {
     if(!sessions.length)
     {
@@ -22,11 +22,17 @@ function onError(error)
     console.error(error);
 }
 
+function restoreMostRecent()
+{
+    var Sessions = browser.sessions.getRecentlyClosed({maxResults: 1});
+    Sessions.then(restoreMostRecentFrom, onError);
+}
+
 const Cmds = browser.commands.getAll();
 Cmds.then(function(cmds) {
     for(let cmd of cmds)
     {
-        console.log("Registered command " + cmd.name);
+        console.debug("Registered command " + cmd.name);
     }
 });
 
@@ -34,9 +40,55 @@ Cmds.then(function(cmds) {
 browser.commands.onCommand.addListener(function(command) {
     if(command == "undo-close-session")
     {
-        console.log("Command triggered: " + command);
+        console.debug("Command triggered: " + command);
+        restoreMostRecent();
+    }
+});
 
-        var Sessions = browser.sessions.getRecentlyClosed({maxResults: 1});
-        Sessions.then(restoreMostRecent, onError);
+// Context menu
+function createContextMenuItem()
+{
+    browser.menus.create({
+        id: "UndoTabClose",
+        title: "Reopen Last Tab",
+        contexts: ["all"]
+    }, null);
+
+}
+
+function removeContextMenuItem()
+{
+    browser.menus.remove("UndoTabClose");
+}
+
+browser.menus.onClicked.addListener(function(info, tab) {
+    switch (info.menuItemId)
+    {
+        case "UndoTabClose":
+        console.debug("Menu clicked: " + info.menuItemId);
+        restoreMostRecent();
+        break;
+
+        default:
+        console.error("Unknown menu ID: " + info.menuItemId);
+    }
+});
+
+var ContextMenu = browser.storage.sync.get('InContextMenu');
+ContextMenu.then(function(Result){
+    if(Result.hasOwnProperty("InContextMenu"))
+    {
+        if(Result.InContextMenu == true)
+        {
+            createContextMenuItem();
+        }
+        else
+        {
+            removeContextMenuItem();
+        }
+    }
+    else
+    {
+        createContextMenuItem();
     }
 });
--- options.js
+++ options.js
@@ -46,10 +46,49 @@ async function resetShortcut()
     updateUI();
 }
 
+// Context menu
+function createContextMenuItem()
+{
+    browser.menus.create({
+        id: "UndoTabClose",
+        title: "Reopen Last Tab",
+        contexts: ["all"]
+    }, null);
+
+}
+
+function removeContextMenuItem()
+{
+    browser.menus.remove("UndoTabClose");
+}
+
+// Pref persistence
+function loadOptions()
+{
+    var ContextMenu = browser.storage.sync.get('InContextMenu');
+    ContextMenu.then(function(Result){
+        if(Result.hasOwnProperty("InContextMenu"))
+        {
+            document.getElementById("CheckContextMenu").checked = Result.InContextMenu;
+        }
+        else
+        {
+            document.getElementById("CheckContextMenu").checked = true;
+        }
+    });
+}
+
+function saveOptions()
+{
+    browser.storage.sync.set({'InContextMenu': document.getElementById("CheckContextMenu").checked});
+}
+
 /**
  * Update the UI when the page loads.
  */
 document.addEventListener('DOMContentLoaded', updateUI);
+document.addEventListener('DOMContentLoaded', loadOptions);
+
 
 /**
  * Handle update and reset button clicks
@@ -57,3 +96,15 @@ document.addEventListener('DOMContentLoaded', updateUI);
 // document.querySelector('#update').addEventListener('click', updateShortcut)
 document.querySelector('#shortcut').addEventListener('input', updateShortcut)
 document.querySelector('#reset').addEventListener('click', resetShortcut)
+
+document.getElementById("CheckContextMenu").addEventListener('click', function(event){
+    saveOptions();
+    if(event.target.checked)
+    {
+        createContextMenuItem();
+    }
+    else
+    {
+        removeContextMenuItem();
+    }
+});
